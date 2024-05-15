6874246e99bb2b31e422dc9baea4df994b5e5dc6
hieudxx <dxhieu03@gmail.com>
2024-05-15 23:33:25 +0700

    update prodt

--- app/src/main/java/fpoly/md16/depotlife/Helper/Interfaces/Api/ApiProduct.java
+++ app/src/main/java/fpoly/md16/depotlife/Helper/Interfaces/Api/ApiProduct.java
@@ -29,16 +29,15 @@ public interface ApiProduct {
     @Multipart
     @POST("products/update/{id}")
     Call<List<String>> update(@Header("Authorization") String authToken,
-                         @Path("id") int id,
-                         @Part("product_name") RequestBody name,
-                         @Part("sell_price") RequestBody export_price,
-                         @Part("import_price") RequestBody import_price,
-                         @Part("total_quantity") RequestBody inventory,
-                         @Part("unit") RequestBody unit,
-                         @Part("supplier_id") RequestBody supplier_id,
-                         @Part("categories_id") RequestBody categories_id,
-                         @Part("pin_image") RequestBody pin_image,
-                         @Part MultipartBody.Part[] images
+                              @Path("id") int id,
+                              @Part("product_name") RequestBody name,
+                              @Part("sell_price") RequestBody export_price,
+                              @Part("import_price") RequestBody import_price,
+                              @Part("unit") RequestBody unit,
+                              @Part("supplier_id") RequestBody supplier_id,
+                              @Part("categories_id") RequestBody categories_id,
+                              @Part("pin_image") RequestBody pin_image,
+                              @Part MultipartBody.Part[] images
     );
 
     @Multipart
@@ -55,5 +54,9 @@ public interface ApiProduct {
 
     @GET("products/delete/{id}")
     Call<Product> delete(@Header("Authorization") String authToken, @Path("id") int id);
+
+    @GET("products/delete-image/{id}")
+    Call<Product> deleteImage(@Header("Authorization") String authToken, @Path("id") int id);
+
 
 }
--- app/src/main/java/fpoly/md16/depotlife/Product/Adapter/ProductImagesAdapter.java
+++ app/src/main/java/fpoly/md16/depotlife/Product/Adapter/ProductImagesAdapter.java
@@ -5,28 +5,38 @@ import android.content.res.ColorStateList;
 import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.squareup.picasso.Picasso;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import fpoly.md16.depotlife.Helper.Helper;
 import fpoly.md16.depotlife.Helper.Interfaces.Api.API;
+import fpoly.md16.depotlife.Helper.Interfaces.Api.ApiProduct;
 import fpoly.md16.depotlife.Helper.Interfaces.onClickListener.onItemRcvClick;
 import fpoly.md16.depotlife.Product.Model.Image;
+import fpoly.md16.depotlife.Product.Model.Product;
 import fpoly.md16.depotlife.databinding.ItemImagesProductBinding;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class ProductImagesAdapter extends RecyclerView.Adapter<ProductImagesAdapter.ViewHolder> {
     private Context context;
     private List<Image> list;
     private onItemRcvClick onItemRcvClick;
+    private String token;
 
-    public ProductImagesAdapter(Context context, List<Image> list, onItemRcvClick onItemRcvClick) {
+    public ProductImagesAdapter(Context context, List<Image> list, onItemRcvClick onItemRcvClick, String token) {
         this.context = context;
-        this.list = list;
+        this.list = new ArrayList<>(list);
         this.onItemRcvClick = onItemRcvClick;
+        this.token = token;
     }
 
     @NonNull
@@ -38,10 +48,11 @@ public class ProductImagesAdapter extends RecyclerView.Adapter<ProductImagesAdap
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        if (list.get(position).getIs_pined() == 1) holder.binding.imgProduct.setStrokeColor(ColorStateList.valueOf(Color.GRAY));
+        if (list.get(position).getIs_pined() == 1)
+            holder.binding.imgProduct.setStrokeColor(ColorStateList.valueOf(Color.GRAY));
         else holder.binding.imgProduct.setStrokeColor(ColorStateList.valueOf(Color.TRANSPARENT));
 
-        Picasso.get().load(API.URL_IMG + list.get(position).getPath().replace("public","")).into(holder.binding.imgProduct);
+        Picasso.get().load(API.URL_IMG + list.get(position).getPath().replace("public", "")).into(holder.binding.imgProduct);
 
         holder.itemView.setOnClickListener(view -> {
             for (int i = 0; i < list.size(); i++) {
@@ -55,7 +66,25 @@ public class ProductImagesAdapter extends RecyclerView.Adapter<ProductImagesAdap
         });
 
         holder.binding.imgDelete.setOnClickListener(view -> {
+            Helper.onCheckdeleteDialog(context, () -> {
+                ApiProduct.apiProduct.deleteImage(token, list.get(position).getId()).enqueue(new Callback<Product>() {
+                    @Override
+                    public void onResponse(Call<Product> call, Response<Product> response) {
+                        if (response.isSuccessful() || response.code() == 200) {
+                            Toast.makeText(context, "Xóa thành công", Toast.LENGTH_SHORT).show();
+                            list.remove(position);
+                            notifyItemRemoved(position);
+                            notifyItemRangeChanged(position, list.size());
+                        }
 
+                    }
+
+                    @Override
+                    public void onFailure(Call<Product> call, Throwable throwable) {
+
+                    }
+                });
+            });
         });
     }
 
